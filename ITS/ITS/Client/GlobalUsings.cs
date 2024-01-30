9b4492f5c7128224679776628e3c894a307a23b8
RanaGowdA <138612260+RanaGowdA@users.noreply.github.com>
2024-01-30 18:51:33 +0000

    Added Mud Blazor Nuget Added UI MainLayout ; Nav Menu ;Template Holders Global Usings

---
+++ ITS/ITS/Client/GlobalUsings.cs
@@ -0,0 +1,1 @@
+﻿global using ITS.Client.Pages.MainLayout;
--- ITS/ITS/Client/Program.cs
+++ ITS/ITS/Client/Program.cs
@@ -1,6 +1,7 @@
 using ITS.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,10 @@ builder.Services.AddHttpClient("ITS.ServerAPI", client => client.BaseAddress = n
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ITS.ServerAPI"));
+
+#region UI Service
+builder.Services.AddMudServices();
+#endregion UI Service
+
 
 await builder.Build().RunAsync();
